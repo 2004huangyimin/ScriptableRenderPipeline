0418ebe18a94278a28e29b2becd085a6d70dde37
Jean-Francois Fortin <jffortin@unity3d.com>
2018-04-03 17:56:47 -0400

    Created StackLit material.

---
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/StackLit/StackLit.cs.hlsl
@@ -0,0 +1,61 @@
+//
+// This file was automatically generated. Please don't edit by hand.
+// HAND EDITED FOR NOW... NEED TO REGENERATE FOR STACKLIT
+//
+
+#ifndef STACKLIT_CS_HLSL
+#define STACKLIT_CS_HLSL
+//
+// UnityEngine.Experimental.Rendering.HDPipeline.StackLit+SurfaceData:  static fields
+//
+#define DEBUGVIEW_STACKLIT_SURFACEDATA_COLOR (1100)
+
+//
+// UnityEngine.Experimental.Rendering.HDPipeline.StackLit+BSDFData:  static fields
+//
+#define DEBUGVIEW_STACKLIT_BSDFDATA_COLOR (1130)
+
+// Generated from UnityEngine.Experimental.Rendering.HDPipeline.StackLit+SurfaceData
+// PackingRules = Exact
+struct SurfaceData
+{
+    float3 color;
+};
+
+// Generated from UnityEngine.Experimental.Rendering.HDPipeline.StackLit+BSDFData
+// PackingRules = Exact
+struct BSDFData
+{
+    float3 color;
+};
+
+//
+// Debug functions
+//
+void GetGeneratedSurfaceDataDebug(uint paramId, SurfaceData surfacedata, inout float3 result, inout bool needLinearToSRGB)
+{
+    switch (paramId)
+    {
+        case DEBUGVIEW_STACKLIT_SURFACEDATA_COLOR:
+            result = surfacedata.color;
+            needLinearToSRGB = true;
+            break;
+    }
+}
+
+//
+// Debug functions
+//
+void GetGeneratedBSDFDataDebug(uint paramId, BSDFData bsdfdata, inout float3 result, inout bool needLinearToSRGB)
+{
+    switch (paramId)
+    {
+        case DEBUGVIEW_STACKLIT_BSDFDATA_COLOR:
+            result = bsdfdata.color;
+            needLinearToSRGB = true;
+            break;
+    }
+}
+
+
+#endif
